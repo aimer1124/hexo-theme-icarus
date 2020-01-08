f72321a7b02093428a491def697e021a045baba1
Yuanjie <380774061@qq.com>
2020-01-08 15:46:53 +0800

    Fix WeChat pic

--- layout/widget/toc.locals.js
+++ layout/widget/toc.locals.js
@@ -2,7 +2,6 @@ module.exports = (ctx, locals) => {
     const { layout, content } = ctx.page;
     const { get_config } = ctx;
     if (layout !== 'page' && layout !== 'post') {
-    // if (get_config('toc') !== true || (layout !== 'page' && layout !== 'post')) {
         return null;
     }
     return Object.assign(locals, { content });
